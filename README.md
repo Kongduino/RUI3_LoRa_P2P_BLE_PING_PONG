b94da1801f21445a0b5b627c52ba5811c552065a
Kongduino <dbvic@me.com>
2022-04-07 14:02:38 +0800

    Comments. Added one command.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # RUI3_LoRa_P2P_BLE_PING_PONG
 
 A BLE-enabled PING-PONG LoRa P2P sketch for RUI3 / RAK4631.
-It accepts two commands, either via Serial or BLE: `/whomai` and `/ping`. They are self-explanatory.
+It accepts three commands, either via Serial or BLE:
+
+* `/whomai`: get the BLE broadcast name. Useful when you have a few devices. You entre this command on Serial, and get the right name.
+* `/ping`: self-explanatory I believe...
+* `/> xxxxx`: send a custom message. Notice the space between `/>` and `xxxxxx`.
 
 ![Serial_Screenshot](Serial_Screenshot.png)
 ![BLE_Screenshot](BLE_Screenshot.jpg)
--- RUI3_LoRa_P2P_BLE_PING_PONG.ino
+++ RUI3_LoRa_P2P_BLE_PING_PONG.ino
@@ -57,6 +57,7 @@ void recv_cb(rui_lora_p2p_recv_t data) {
   char msg[92];
   sprintf(msg, "Incoming message, length: %d, RSSI: %d, SNR: %d\n", data.BufferSize, data.Rssi, data.Snr);
   Serial.print(msg);
+  // Adafruit's Bluefruit connect is being difficult, and seems to require \r\n to fully receive a message..
   sprintf(msg, "[%d] RSSI %d SNR %d\r\n", data.BufferSize, data.Rssi, data.Snr);
   api.ble.uart.write((uint8_t*)msg, strlen(msg));
   hexDump(data.Buffer, data.BufferSize);
@@ -67,28 +68,37 @@ void recv_cb(rui_lora_p2p_recv_t data) {
 
 void send_cb(void) {
   // TX callback
-  Serial.printf("P2P set Rx mode %s\r\n", api.lorawan.precv(65534) ? "Success" : "Fail");
+  Serial.printf("Set infinite Rx mode %s\r\n", api.lorawan.precv(65534) ? "Success" : "Fail");
   // set the LoRa module to indefinite listening mode:
   // no timeout + no limit to the number of packets
   // NB: 65535 = wait for ONE packet, no timeout
 }
 
 void sendPing() {
-  api.lorawan.precv(0);
   char payload[32];
-  char msg[32];
   sprintf(payload, "PING #0x%04x", counter++);
-  sprintf(msg, "P2P send %s: %s\n\r", payload, api.lorawan.psend(strlen(payload), (uint8_t*)payload) ? "Success" : "Fail");
+  sendMsg(payload);
+}
+
+void sendMsg(char* payload) {
+  uint8_t ln = strlen(payload);
+  api.lorawan.precv(0);
+  // turn off reception – a little hackish, but without that send might fail.
+  char msg[ln + 18];
+  sprintf(msg, "Sending `%s`: %s\n\r", payload, api.lorawan.psend(ln, (uint8_t*)payload) ? "Success" : "Fail");
+  ln = strlen(msg);
   Serial.print(msg);
   Serial.println("Sending to BLE...");
-  uint16_t ln = strlen(msg);
   api.ble.uart.write((uint8_t*)msg, ln);
 }
 
 void setup() {
   Serial.begin(115200, RAK_CUSTOM_MODE);
+  // RAK_CUSTOM_MODE disables AT firmware parsing
   time_t timeout = millis();
   while (!Serial) {
+    // on nRF52840, Serial is not available right away.
+    // make the MCU wait a little
     if ((millis() - timeout) < 5000) {
       delay(100);
     } else {
@@ -99,11 +109,10 @@ void setup() {
   while (x > 0) {
     delay(500);
     Serial.printf("%d, ", x--);
-  }
+  } // Just for show
   Serial.println("0!");
   Serial.println("RAKwireless LoRaWan P2P BLE Example");
   Serial.println("------------------------------------------------------");
-  delay(2000);
   startTime = millis();
   Serial.println("P2P Start");
   Serial.printf("Hardware ID: %s\r\n", api.system.chipId.get().c_str());
@@ -111,6 +120,8 @@ void setup() {
   Serial.printf("RUI API Version: %s\r\n", api.system.apiVersion.get().c_str());
   Serial.printf("Firmware Version: %s\r\n", api.system.firmwareVersion.get().c_str());
   Serial.printf("AT Command Version: %s\r\n", api.system.cliVersion.get().c_str());
+
+  // LoRa setup – everything else has been done for you. No need to fiddle with pins, etc
   Serial.printf("Set work mode to P2P: %s\r\n", api.lorawan.nwm.set(0) ? "Success" : "Fail");
   Serial.printf("Set P2P frequency to %3.3f: %s\r\n", (myFreq / 1e6), api.lorawan.pfreq.set(myFreq) ? "Success" : "Fail");
   Serial.printf("Set P2P spreading factor to %d: %s\r\n", sf, api.lorawan.psf.set(sf) ? "Success" : "Fail");
@@ -118,14 +129,18 @@ void setup() {
   Serial.printf("Set P2P code rate to 4/%d: %s\r\n", (cr + 5), api.lorawan.pcr.set(0) ? "Success" : "Fail");
   Serial.printf("Set P2P preamble length to %d: %s\r\n", preamble, api.lorawan.ppl.set(8) ? "Success" : "Fail");
   Serial.printf("Set P2P TX power to %d: %s\r\n", txPower, api.lorawan.ptp.set(22) ? "Success" : "Fail");
+
+  // LoRa callbacks
   api.lorawan.registerPRecvCallback(recv_cb);
   api.lorawan.registerPSendCallback(send_cb);
 
   // api.system.restoreDefault();
   // This causes various issues. Including a reboot. Let's stay away from that.
-  
-  // If you want to read and write data through BLE API operations, you need to set BLE Serial (Serial6) to Custom Mode
+
   Serial6.begin(115200, RAK_CUSTOM_MODE);
+  // If you want to read and write data through BLE API operations,
+  // you need to set BLE Serial (Serial6) to Custom Mode
+
   uint8_t pairing_pin[] = "004631";
   Serial.print("Setting pairing PIN to: ");
   Serial.println((char *)pairing_pin);
@@ -134,12 +149,14 @@ void setup() {
   // This will cause apps to perform pairing with static PIN we set above
   // now support SET_ENC_WITH_MITM and SET_ENC_NO_MITM
   api.ble.uart.setPermission(RAK_SET_ENC_WITH_MITM);
+
   char ble_name[] = "3615_My_RAK"; // You have to be French to understand this joke
   Serial.print("Setting Broadcast Name to: ");
   Serial.println(ble_name);
   api.ble.settings.broadcastName.set(ble_name, strlen(ble_name));
   api.ble.uart.start();
   api.ble.advertise.start(0);
+
   // This version doesn't have an automatic Tx functionality:
   // YOU are in charge of sending, either via Serial or BLE.
 }
@@ -151,6 +168,12 @@ void handleCommands(char *cmd) {
     sendPing();
     return;
   }
+
+  if (cmd[1] == '>' && cmd[2] == ' ') {
+    sendMsg(cmd + 3);
+    return;
+  }
+
   if (strcmp(cmd, "/whoami") == 0) {
     char msg[64];
     sprintf(msg, "Broadcast name: %s\n\r", api.ble.settings.broadcastName.get());
@@ -164,16 +187,22 @@ void handleCommands(char *cmd) {
 
 void loop() {
   if (api.ble.uart.available()) {
+    // store the incoming string into a buffer
     Serial.println("\nIncoming:");
     char str1[256];
     uint8_t ix = 0;
+    // with a 256-byte buffer and a uint8_t index
+    // you won't get a buffer overrun :-)
     while (api.ble.uart.available()) {
       char c = api.ble.uart.read();
       if (c > 31) str1[ix++] = c;
+      // strip \r\n and the like
+      // this is ok because we expect text. You might want to adjust if you are sending binary data
     }
     str1[ix] = 0;
     Serial.println(str1);
     handleCommands(str1);
+    // pass teh string to the command-handling fn
   }
   if (Serial.available()) {
     Serial.println("\nIncoming:");
